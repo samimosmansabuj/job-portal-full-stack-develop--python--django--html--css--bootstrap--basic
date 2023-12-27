0156dea5be962f9d846bdfee98cc3b3a488f84f6
Samim Osman <samim.o.sabuj01@gmail.com>
2023-12-27 19:53:10 +0600

    new update

--- delete/job/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-class Job_Admin(admin.ModelAdmin):
-    list_display = ['job_title', 'vacancy', 'employment_status', 'application_deadline']
-
-admin.site.register(Job, Job_Admin)
--- delete/job/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('add-job/', add_job, name='add_job'),
-    path('job-all/', list_job, name='list_job'),
-]
